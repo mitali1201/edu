720001718920c9e4bae0540cabf09bba2fe528a1
Akash Gupta <akashgupta@Akashs-MacBook-Pro.local>
2022-11-11 20:46:48 +0530

    fr

--- src/new-components/Login.jsx
+++ src/new-components/Login.jsx
@@ -1,14 +1,56 @@
 import React from 'react'
 import "./login.css"
-import bg from "./gradient.webp"
 import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
 
 export default function Login() {
 
   const navi = useNavigate();
 
-  const handleLogIn = () => {
-    navi('/home')
+  const emailRef = useRef(null);
+  const passRef = useRef(null);
+
+  const Reset = () => {
+    emailRef.current.value = "";
+    passRef.current.value = "";
+  }
+
+  const handleLogIn = async () => {
+    console.log('inside log in');
+    let email = emailRef.current.value;
+    let password = passRef.current.value;
+    if (email === "" || password === "") {
+      alert('please fill all fields');
+    }
+    else {
+      try {
+        let r = await fetch('http://localhost:9189/getUser', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+        });
+        let r1 = await r.json();
+        console.log('data: ', r1);
+        if (!r1.issue) {
+          let ls = localStorage.setItem("userSetting", JSON.stringify({ loginStatus: true }));
+          navi('/home');
+        } else {
+          if (r1.response === 0) {
+            Reset();
+            alert('either email or password is wrong');
+          }
+          else {
+            Reset();
+            alert('some problem occur during sign in please try again later.');
+          }
+        }
+      } catch (error) {
+        Reset();
+        alert('server error please try again later.');
+      }
+    }
   }
 
   return (
@@ -17,11 +59,11 @@ export default function Login() {
         <h2>Login</h2>
         <form>
           <div class="user-box">
-            <input type="text" name="name" required />
+            <input ref={emailRef} type="text" name="name" required />
             <label>Username</label>
           </div>
           <div class="user-box">
-            <input type="password" name="pass" required />
+            <input ref={passRef} type="password" name="pass" required />
             <label>Password</label>
           </div>
           <div onClick={() => handleLogIn()} className='buttonDesign'>
--- src/new-components/Sign.jsx
+++ src/new-components/Sign.jsx
@@ -1,23 +1,56 @@
 import React from 'react'
-import bg from "./gradient.webp"
-import Home from "./Home.jsx"
-import { Link, useNavigate } from "react-router-dom";
 import "./login.css"
+import { useNavigate } from "react-router-dom";
+import { useRef } from 'react';
 
 
 export default function Sign() {
 
   const navi = useNavigate();
 
-  const handleSignIn = () => {
-    let ls = localStorage.getItem("userSetting");
-    console.log("data from local storage: ", ls);
-    if (ls === null) {
-      // localStorage.setItem("userSetting", "Akash gupta");
-      // navi('/home')
+  const emailRef = useRef(null);
+  const passRef = useRef(null);
+
+  const Reset = () => {
+    emailRef.current.value = "";
+    passRef.current.value = "";
+  }
+
+  const handleSignIn = async () => {
+    console.log('inside sign in')
+    let email = emailRef.current.value;
+    let password = passRef.current.value;
+    if (email === "" || password === "") {
+      alert('please fill all fields');
     }
     else {
-
+      try {
+        let r = await fetch('http://localhost:9189/setUser', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+        })
+        let r1 = await r.json();
+        console.log('data: ', r1);
+        if (!r1.issue) {
+          let ls = localStorage.setItem("userSetting", JSON.stringify({ loginStatus: true }));
+          navi('/home');
+        } else {
+          if (r1.response === 'duplicate') {
+            Reset();
+            alert('user already registered');
+          }
+          else {
+            Reset();
+            alert('some problem occur during sign in please try again later.');
+          }
+        }
+      } catch (error) {
+        Reset();
+        alert('server error please try again later.');
+      }
     }
   }
 
@@ -27,11 +60,11 @@ export default function Sign() {
         <h2>Login</h2>
         <form>
           <div class="user-box">
-            <input type="text" name="name" required />
-            <label>Username</label>
+            <input ref={emailRef} type="text" name="name" required />
+            <label>Email id</label>
           </div>
           <div class="user-box">
-            <input type="password" name="pass" required />
+            <input ref={passRef} type="password" name="pass" required />
             <label>Password</label>
           </div>
           <div onClick={() => handleSignIn()} className='buttonDesign' >
